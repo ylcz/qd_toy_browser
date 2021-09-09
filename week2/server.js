1ff1f4a1cab0db92d376a1976309db931338947f
ylcz <ylcz@outlook.com>
2021-09-09 09:20:06 +0800

    整理目录，开始第二周

---
+++ week2/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log("request received");
+    console.log(req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("x-Foo", "bar");
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end(
+`<html maaa=a >
+<head>
+    <style>
+body div #myid{
+    width: 100px;
+    background-color: #ff5000;
+}
+body div img{
+    width: 30px;
+    background-color: #ff1111;
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid"/>
+        <img />
+    </div>
+</body>
+</html>
+`);
+}).listen(8088);
+
+console.log('server started\n');
