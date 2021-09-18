77baef2648d4294d6c4652217df9711504605fce
ylcz <ylcz@outlook.com>
2021-09-18 13:45:52 +0800

    浏览器工作原理：排版渲染 第三周作业

---
+++ week3/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    console.log("request received");
+    console.log(req.headers);
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("x-Foo", "bar");
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end(
+`<html maaa=a >
+<head>
+    <style>
+#container {
+    width: 500px;
+    height: 300px;
+    display: flex;
+    background-color: rgb(255,255,255);
+}
+#container #myid{
+    width: 200px;
+    height: 100px;
+    background-color: rgb(255,0,0);
+}
+#container .c1{
+    flex: 1;
+    background-color: rgb(0,255,0);
+}
+    </style>
+</head>
+<body>
+    <div id="container">
+        <div id="myid"></div>
+        <div class="c1"></div>
+    </div>
+</body>
+</html>`);
+}).listen(8088);
+
+console.log('server started\n');
